3bcfa0c57d3918c206edfbd8a375756dfb5a0181
Diego Pereira Betti <diegopbetti.gmail.com>
2025-12-17 15:27:26 -0300

    Constroi pipeline de pos-processamento

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,22 @@
 from bciflow.datasets.cbcic import cbcic
 from bciflow.modules.tf.filterbank import filterbank
+from bciflow.modules.sf.csp import csp
+from bciflow.modules.fe.logpower import logpower
+from bciflow.modules.fs.mibif import MIBIF
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
 
 dataset = cbcic(subject=1, path='data/cbcic/')
 
 pre_folding = {'tf': (filterbank, {'kind_bp': 'chebyshevII'})}
+
+sf = csp()
+fe = logpower
+fs = MIBIF(8, clf=lda())
+clf = lda()
+
+pos_folding = {
+    'sf': (sf, {}),
+    'fe': (fe, {}),
+    'fs': (fs, {}),
+    'clf': (clf, {})
+}
