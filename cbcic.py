0391bda58c4b40f12b36a5923df992a7c144fe6f
Diego Pereira Betti <diegopbetti.gmail.com>
2025-12-18 13:48:57 -0300

    Adiciona comentarios

--- cbcic.py
+++ cbcic.py
@@ -2,10 +2,23 @@ from bciflow.datasets.cbcic import cbcic
 
 dataset = cbcic(subject=1, path='data/cbcic/')
 
+# dimensões do array principal, contendo os sinais de EEG(trials, bandas, quantidade de eletrodos e tempo)
 print("EEG signals shape:", dataset["X"].shape)
+
+# sequência de rótulos de classe (inteiros) para cada trial do dataset 
 print("Labels:", dataset["y"])
+
+# dicionário que traduz os rótulos numéricos para nomes de classes descritivos
 print("Class dictionary:", dataset["y_dict"])
+
+# marcadores de tempo de eventos importantes dentro das trials
 print("Events:", dataset["events"])
+
+# lista dos nomes dos eletrodos de EEG utilizados na coleta dos dados
 print("Channel names:", dataset["ch_names"])
+
+# taxa em Hertz na qual os sinais de EEG foram digitalizados (amostras por segundo)
 print("Sampling frequency (Hz):", dataset["sfreq"])
+
+# ponto de partida temporal do segmento de EEG em relação ao evento principal do trial
 print("Start time (s):", dataset["tmin"])
--- data_processing.py
+++ data_processing.py
@@ -1,22 +1,38 @@
 from bciflow.datasets.cbcic import cbcic
+# banco de filtros temporais aos sinais EEG
 from bciflow.modules.tf.filterbank import filterbank
+
+# realiza o algoritmo Common Spatial Patterns (filtros espaciais)
 from bciflow.modules.sf.csp import csp
+
+# extrai a potência logarítmica como característica dos dados
 from bciflow.modules.fe.logpower import logpower
+
+# realiza a seleção de características baseada em informação mútua
 from bciflow.modules.fs.mibif import MIBIF
+
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
+
+# executa a validação cruzada k-fold do pipeline
 from bciflow.modules.core.kfold import kfold
+
 import pandas as pd
+#  calcula a acurácia de classificação dos resultados
 from bciflow.modules.analysis.metric_functions import accuracy
 
 dataset = cbcic(subject=1, path='data/cbcic/')
 
+# Define a etapa de pré-processamento para aplicar um banco de filtros
 pre_folding = {'tf': (filterbank, {'kind_bp': 'chebyshevII'})}
 
+# Instancia os módulos de processamento espacial (CSP),
+#  extração de características (logpower), seleção de características (MIBIF) e o classificador (LDA)
 sf = csp()
 fe = logpower
 fs = MIBIF(8, clf=lda())
 clf = lda()
 
+# Organiza as etapas de processamento que serão aplicadas dentro de cada fold de validação cruzada
 pos_folding = {
     'sf': (sf, {}),
     'fe': (fe, {}),
@@ -24,6 +40,8 @@ pos_folding = {
     'clf': (clf, {})
 }
 
+# Executa o pipeline completo 
+# (pré-processamento e processamento) usando validação cruzada k-fold no dataset
 results = kfold(
     target=dataset,
     start_window=dataset['events']['cue'][0] + 0.5,
@@ -31,9 +49,13 @@ results = kfold(
     pos_folding=pos_folding
 )
 
+# Converte os resultados detalhados de cada fold em um DataFrame do pandas para facilitar a análise
 df = pd.DataFrame(results)
+# Calcula a acurácia média do modelo a partir dos resultados de todos os folds
 acc = accuracy(df)
 
+# Imprime dataframe completo com os resultados detalhados de cada fold
 print(df)
 
+# Imprime a acurácia final calculada para o pipeline FBCSP, formatada com 4 casas decimais
 print(f"Accuracy: {acc:.4f}")
